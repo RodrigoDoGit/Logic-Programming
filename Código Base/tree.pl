b41a159afaaae901746c9dd6bd23d70ba43197ba
Rodrigo Emídio <94897828+RodrigoDoGit@users.noreply.github.com>
2022-07-20 19:03:38 +0100

    Add files via upload

---
+++ Código Base/tree.pl
@@ -0,0 +1,55 @@
+
+husband(arthur, beryl).
+father(arthur, carol).
+father(arthur, david).
+father(arthur, elaine).
+father(arthur, frank).
+
+female(beryl).
+
+female(carol).
+
+female(elaine).
+
+husband(frank, glenda).
+father(frank, linda).
+father(frank, malcolm).
+
+female(glenda).
+
+female(helen).
+
+husband(ian, natalie).
+father(ian, oscar).
+
+husband(john, katherine).
+father(john, glenda).
+father(john, helen).
+father(john, ian).
+
+female(katherine).
+
+female(linda).
+
+female(natalie).
+
+husband(paul, linda).
+father(paul, quentin).
+father(paul, robert).
+
+husband(stephen, carol).
+father(stephen, tom).
+father(stephen, ursula).
+father(stephen, violet).
+
+female(ursula).
+
+female(violet).
+
+husband(william, violet).
+father(william, xavier).
+
+female(yvonne).
+
+husband(zaccharia, yvonne).
+father(zaccharia, william).
